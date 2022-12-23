68dc62208d5a4c7fa3536e3846ad2548d3454b61
MGibbs414 <120430401+MGibbs414@users.noreply.github.com>
2022-12-23 14:02:47 -0500

    plz work read more read less x2

--- resume/public_html/assets/js/johndoe.js
+++ resume/public_html/assets/js/johndoe.js
@@ -154,10 +154,10 @@ function resumePDF(){
   document.location.herf + "www.mgibbs414.github.io/resume/public_html/assets/MeganGresume.pdf";
 };
 
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+function myFunction1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
@@ -170,6 +170,20 @@ function myFunction() {
   }
 }
 
+function myFunction2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
 
 //End of my code
