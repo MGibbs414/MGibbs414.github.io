2fa97bf3e6c5fe60ad7905ed988906da721d5aa2
MGibbs414 <120430401+MGibbs414@users.noreply.github.com>
2022-12-26 13:47:19 -0500

    fuck this shit

--- resume/public_html/assets/js/johndoe.js
+++ resume/public_html/assets/js/johndoe.js
@@ -13,6 +13,8 @@
 */
 
 // smooth scroll
+const fs = require('fs');
+
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
@@ -199,23 +201,7 @@ let saveFile = () => {
       '\r Name: ' + name.value + ' \r\n ' + 
       'Email: ' + email.value + ' \r\n ' + 
       'Message: ' + msg.value;
-
-  // Convert the text to BLOB.
-  const textToBLOB = new Blob([data], { type: 'text/plain' });
-  const sFileName = 'formData.txt';	   // The file to save the data.
-
-  let newLink = document.createElement("a");
-  newLink.download = sFileName;
-
-  if (window.webkitURL != null) {
-    newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-  }
-  else {
-    newLink.href = window.URL.createObjectURL(textToBLOB);
-    newLink.style.display = "none";
-    document.body.appendChild(newLink);
-  }
-
-  newLink.click(); 
+ 
+  fs.writeFileSync('formData.txt',data);    
 }
 //End of my code
