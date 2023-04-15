2cf0fdc41e15aeed032186a4830c0faa323cf96a
MGibbs414 <Purplepenguin414@gmail.com>
2023-04-15 16:40:10 -0400

    help

--- resume/public_html/assets/js/johndoe.js
+++ resume/public_html/assets/js/johndoe.js
@@ -192,6 +192,37 @@ function myFunction3() {
   var dots = document.getElementById("dots3");
   var moreText = document.getElementById("more3");
   var btnText = document.getElementById("myBtn3");
+<<<<<<< Updated upstream
+=======
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction4() {
+  var dots = document.getElementById("dots4");
+  var moreText = document.getElementById("more4");
+  var btnText = document.getElementById("myBtn4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+>>>>>>> Stashed changes
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
